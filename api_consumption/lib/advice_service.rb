5eb07d2082a6a3f1700908ca78aa7f70fef9b2f4
Nathan Keller <keller.nathan@gmail.com>
2020-10-19 11:59:48 -0600

    Create search method to match query params to Advice search endpoint

--- api_consumption/lib/advice_service.rb
+++ api_consumption/lib/advice_service.rb
@@ -4,10 +4,18 @@ require 'faraday'
 class AdviceService
 
   def random
-    response = Faraday.get 'https://api.adviceslip.com/advice'
+    response = self.connection.get('/advice')
     JSON.parse(response.body)
   end
 
-  
+  def search(query)
+    response = self.connection.get("/advice/search/#{query}")
+    JSON.parse(response.body)
+  end
 
+  def connection
+    conn = Faraday.new(
+      url: 'https://api.adviceslip.com/'
+      )
+  end
 end
